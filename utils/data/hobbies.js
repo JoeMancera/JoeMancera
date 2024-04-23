dc779cdc2f4b46e4475ec510746929a9355c15c3
Joe Mancera <joeharrymancera@gmail.com>
2024-04-23 16:30:32 -0500

    feat: add projects and move images to folders

--- utils/data/hobbies.js
+++ utils/data/hobbies.js
@@ -1,22 +1,22 @@
 export const hobbies = [
   {
     title: "Learn",
-    image: "hobby-learn.png",
+    image: "hobbies/hobby-learn.png",
     description: ""
   },
   {
     title: "Photography",
-    image: "hobby-photography.png",
+    image: "hobbies/hobby-photography.png",
     description: ""
   },
   {
     title: "Listen to music",
-    image: "hobby-music.png",
+    image: "hobbies/hobby-music.png",
     description: ""
   },
   {
     title: "See car races",
-    image: "hobby-f1.png",
+    image: "hobbies/hobby-f1.png",
     description: ""
   },
 ]
--- utils/data/projects.js
+++ utils/data/projects.js
@@ -4,20 +4,23 @@ export const projects = [
     description: "Game created for the numerica-jam. Game inspired by the original numerica game created by @rothiotome for twitch.",
     image: "numerica-combo-game.png",
     link: "https://github.com/JoeMancera/numerica-combo-game",
+    code: "https://github.com/JoeMancera/numerica-combo-game",
     technologies: ["Preact", "Vite", "Tailwind CSS", "tmi", "Wouter"]
   },
   {
     title: "Formula fan",
     description: "Site and API scraping information from the official F1 site. This is for educational purposes. I intend to learn how, with Astro, to create a site that has if API and how to get information from a site like Formula 1.",
-    image: "numerica-combo-game.png",
+    image: "formula-fan-page.png",
     link: "https://github.com/JoeMancera/formula-fan",
+    code : "https://github.com/JoeMancera/formula-fan",
     technologies: ["Astro", "Vite", "Tailwind CSS", "Cheerio"]
   },
   {
-    title: "Hackernews app",
+    title: "HackerNews app",
     description: "App to show a list of the recent Hacker News posts about React, Angular and Vue. The user can filter for a specific topic and save the post to favorites list.",
-    image: "numerica-combo-game.png",
+    image: "hackernews-app.png",
     link: "https://github.com/JoeMancera/hackernews-app",
+    code: "https://github.com/JoeMancera/hackernews-app",
     technologies: ["React", "Wouter", "CSS"]
   },
 
